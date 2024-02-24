02535e1793fb96cfc64bd7604b20c32e3b3bd096
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-02-24 17:56:52 -0500

    Updating training script to match notebook

--- examples/lightning/train.py
+++ examples/lightning/train.py
@@ -1,40 +1,20 @@
 import os
-import argparse
-import multiprocessing
-from pathlib import Path
-from PIL import Image
-
 import torch
 from torchvision import models, transforms
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 
 from byol_pytorch import BYOL
 import pytorch_lightning as pl
 
-# test model, a resnet 50
+from torchvision import datasets
 
-resnet = models.resnet50(pretrained=True)
+BATCH_SIZE = 256
+EPOCHS = 2
+LR = 3e-4
+NUM_GPUS = int(os.environ["SLURM_GPUS_ON_NODE"])
+IMAGE_SIZE = 224
+NUM_WORKERS = int(os.environ['SLURM_CPUS_PER_TASK'])
 
-# arguments
-
-parser = argparse.ArgumentParser(description='byol-lightning-test')
-
-parser.add_argument('--image_folder', type=str, required = True,
-                       help='path to your folder of images for self-supervised learning')
-
-args = parser.parse_args()
-
-# constants
-
-BATCH_SIZE = 32
-EPOCHS     = 1000
-LR         = 3e-4
-NUM_GPUS   = 2
-IMAGE_SIZE = 256
-IMAGE_EXTS = ['.jpg', '.png', '.jpeg']
-NUM_WORKERS = multiprocessing.cpu_count()
-
-# pytorch lightning module
 
 class SelfSupervisedLearner(pl.LightningModule):
     def __init__(self, net, **kwargs):
@@ -44,7 +24,8 @@ class SelfSupervisedLearner(pl.LightningModule):
     def forward(self, images):
         return self.learner(images)
 
-    def training_step(self, images, _):
+    def training_step(self, batch, batch_idx):
+        images, labels = batch
         loss = self.forward(images)
         return {'loss': loss}
 
@@ -55,60 +36,55 @@ class SelfSupervisedLearner(pl.LightningModule):
         if self.learner.use_momentum:
             self.learner.update_moving_average()
 
-# images dataset
 
-def expand_greyscale(t):
-    return t.expand(3, -1, -1)
-
-class ImagesDataset(Dataset):
-    def __init__(self, folder, image_size):
+class PredictWrapper(pl.LightningModule):
+    def __init__(self, net, **kwargs):
         super().__init__()
-        self.folder = folder
-        self.paths = []
+        self.learner = net
 
-        for path in Path(f'{folder}').glob('**/*'):
-            _, ext = os.path.splitext(path)
-            if ext.lower() in IMAGE_EXTS:
-                self.paths.append(path)
+    def forward(self, images):
+        return self.learner(images)
 
-        print(f'{len(self.paths)} images found')
+    def predict_step(self, batch, batch_idx, dataloader_idx=0):
+        images, labels = batch
+        return self.forward(images), labels
 
-        self.transform = transforms.Compose([
-            transforms.Resize(image_size),
-            transforms.CenterCrop(image_size),
-            transforms.ToTensor(),
-            transforms.Lambda(expand_greyscale)
-        ])
 
-    def __len__(self):
-        return len(self.paths)
+def setup_imagenet_datasets(root):
+    transform = transforms.Compose([
+        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+        transforms.ToTensor(),
+    ])
 
-    def __getitem__(self, index):
-        path = self.paths[index]
-        img = Image.open(path)
-        img = img.convert('RGB')
-        return self.transform(img)
+    ds_train = datasets.ImageNet(root=root, split='train', transform=transform)
+    train_loader = DataLoader(ds_train, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=True)
 
-# main
+    ds_test = datasets.ImageNet(root=root, split='val', transform=transform)
+    ds_test = torch.utils.data.Subset(ds_test, list(range(500)))
+    test_loader = DataLoader(ds_test, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False)
+
+    return train_loader, test_loader
+
 
 if __name__ == '__main__':
-    ds = ImagesDataset(args.image_folder, IMAGE_SIZE)
-    train_loader = DataLoader(ds, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=True)
+    resnet = models.resnet50(pretrained=True)
+
+    train_loader, test_loader = setup_imagenet_datasets(
+        '/scratch/gpfs/DATASETS/imagenet/ilsvrc_2012_classification_localization')
 
     model = SelfSupervisedLearner(
         resnet,
-        image_size = IMAGE_SIZE,
-        hidden_layer = 'avgpool',
-        projection_size = 256,
-        projection_hidden_size = 4096,
-        moving_average_decay = 0.99
+        image_size=IMAGE_SIZE,
+        hidden_layer='avgpool',
+        projection_size=256,
+        projection_hidden_size=4096,
+        moving_average_decay=0.99
     )
-
     trainer = pl.Trainer(
-        gpus = NUM_GPUS,
-        max_epochs = EPOCHS,
-        accumulate_grad_batches = 1,
-        sync_batchnorm = True
+        gpus=NUM_GPUS,
+        max_epochs=EPOCHS,
+        accumulate_grad_batches=1,
+        sync_batchnorm=True
     )
 
     trainer.fit(model, train_loader)
